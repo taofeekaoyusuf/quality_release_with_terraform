218898b786efdbbeaf3c6ae5c2b20efebf3d0e61
taofeekaoyusuf <dftntekniks@gmail.com>
2022-10-09 23:30:47 +0200

    modified:   terraform.tfstate

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -17,7 +17,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   size                = "Standard_B1s"
-  admin_username      = "dhackVM"
+  admin_username      = "myLinuxVM"
   network_interface_ids = [azurerm_network_interface.test.id,]
 
   admin_ssh_key {
